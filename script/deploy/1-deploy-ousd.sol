900cd43cca150b5aeaadbc871c772212dcdc5a34
Clément <clemmoller@gmail.com>
2025-04-11 16:44:25 +0200

    Enhance deployment script by adding context handling and forking logic for Mainnet Sepolia

--- script/deploy/1-deploy-ousd.sol
+++ script/deploy/1-deploy-ousd.sol
@@ -2,14 +2,25 @@
 pragma solidity ^0.8.13;
 
 import {Script} from "dependencies/forge-std-1.9.6/src/Script.sol";
+import {Vm, VmSafe} from "dependencies/forge-std-1.9.6/src/Vm.sol";
 
 import {OUSDScript} from "test/deploy/1-deploy-ousd.sol";
 
 contract Deploy_001 is Script, OUSDScript {
+    function setUp() public {}
+
     function run() public {
-        IS_TEST = false;
-        vm.startBroadcast();
-        deploy_1(DeployActions.Create);
+        vm.createSelectFork(vm.envString("MAINNET_SEPOLIA_RPC_URL"));
+
+        vm.isContext(VmSafe.ForgeContext.ScriptBroadcast)
+            ? vm.startBroadcast(vm.envUint("MAINNET_DEPLOYER_PRIVATE_KEY"))
+            : vm.startBroadcast(vm.envAddress("MAINNET_DEPLOYER"));
+
+        _create_1();
         vm.stopBroadcast();
     }
 }
+
+// How to run:
+// forge script script/deploy/1-deploy-ousd.sol --broadcast --verify -vvvv
+//
